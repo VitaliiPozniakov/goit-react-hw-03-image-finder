9bb2a6eee1345546f5d56a495bf22025c8f3ea68
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-04 22:43:53 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar';
+import { fetchImages } from 'services/api';
+import { ToastContainer } from 'react-toastify';
+
+export default class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: null,
+    error: null,
+    isLoading: false,
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+
+    try {
+      if (prevState.query !== query) {
+        this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+        const dataFromBackend = await fetchImages(query);
+        // console.log(dataFromBackend.hits)
+        const { hits } = dataFromBackend;
+        const imagesArray = hits.map(hit => ({
+          id: hit.id,
+          littleImageUrl: hit.webformatURL,
+          largeImageUrl: hit.largeImageURL,
+        }));
+        // console.log(imagesArray)
+        return this.setState(({ isLoading}) => ({
+          isLoading: !isLoading,
+          images: imagesArray,
+        }));
+      }
+
+      if (prevState.page !== page && page !== 1) {
+        this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+        const dataFromBackend = await fetchImages(query, page);
+        // console.log(dataFromBackend.hits)
+        const { hits } = dataFromBackend;
+        const imagesArray = hits.map(hit => ({
+          id: hit.id,
+          littleImageUrl: hit.webformatURL,
+          largeImageUrl: hit.largeImageURL,
+        }));
+        // console.log(imagesArray)
+        return this.setState(({ isLoading, images }) => ({
+          isLoading: !isLoading,
+          images: [...images, ...imagesArray],
+        }));
+      }
+
+
+    } catch (error) {
+      this.setState({ error });
+    }
+  }
+
+
+
+  getSearchRequest = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmitProp={this.getSearchRequest} />
+        <ToastContainer position="top-right" autoClose={3000} />
+      </div>
+    );
+  }
+}
---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api'
+
+const API_KEY = '28004990-f3c49f187ad64f64267c5955f';
+
+
+export const  fetchImages = async (query, page = 1) => {
+
+console.log(page)
+
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 12,
+        page: page,
+      });
+
+const response = await axios.get(`/?${searchParams}`);
+
+// console.log(response.data)
+
+return response.data;
+}
+
