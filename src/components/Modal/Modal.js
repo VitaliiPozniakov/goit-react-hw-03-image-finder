07fbbc241ba6aa8ad50cf6859c918b6b1543d023
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-05 14:32:58 +0300

    add modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
-import { Overlay, Content } from './Modal.styled';
+import { Overlay, Content, IconClose } from './Modal.styled';
+// import { AiFillCloseCircle } from "react-icons/ai";
+// import styled from "@emotion/styled";
 // import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
@@ -14,25 +16,28 @@ export default class Modal extends Component {
     window.removeEventListener(`keydown`, this.handleKeyDown);
   }
 
-
-
   handleKeyDown = e => {
     if (e.code === 'Escape') {
-    //   console.log('нажали єскейп');
+      //   console.log('нажали єскейп');
       this.props.onClose();
     }
   };
 
   handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-    //   console.log('нажали backdrop');
+      //   console.log('нажали backdrop');
       this.props.onClose();
     }
-  }; 
+  };
+
+  handleIconCloseClick = e => {
+    this.props.onClose();
+  };
 
   render() {
     return createPortal(
       <Overlay onClick={this.handleBackdropClick}>
+        <IconClose onClick={this.handleIconCloseClick} />
         <Content>{this.props.children}</Content>
       </Overlay>,
       modalRoot
--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 export const Overlay = styled.div`
 position: fixed;
@@ -16,4 +17,17 @@ z-index: 1200;
 export const Content = styled.div`
 max-width: calc(100vw - 48px);
 max-height: calc(100vh - 24px);
+`;
+
+export const IconClose = styled(AiFillCloseCircle)`
+position: absolute;
+top: 0;
+right: 0;
+width: 32px;
+height:  32px;
+transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+&:hover, &:focus{
+    color: black;
+}
 `;
